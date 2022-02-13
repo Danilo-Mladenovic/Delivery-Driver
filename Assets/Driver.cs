f91b966d5708e42639c6a2012b80444e24b903b0
Danilo-Mladenovic <danilo.mladenovic999@gmail.com>
2022-02-13 17:18:16 +0100

    Added variables and SerializeFields.

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float starting_angle = -90f;
+    [SerializeField] float translation_z = 0.01f;
+
     void Start()
     {
-        transform.Rotate(0, 0, -90);
+        transform.Rotate(0, 0, starting_angle);
     }
 
     void Update()
     {
         // transform.Rotate(0, 0, 1);
-        transform.Translate(0, 0.1f, 0);
+        transform.Translate(0, translation_z, 0);
     }
 }
