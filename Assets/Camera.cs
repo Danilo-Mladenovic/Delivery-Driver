40bb13c73c9d30b078b27297b8d4087c406f845b
Danilo-Mladenovic <danilo.mladenovic999@gmail.com>
2022-02-17 17:51:58 +0100

    Camera now follows the player.

---
+++ Assets/Camera.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] GameObject CameraFollow;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = CameraFollow.transform.position + new Vector3(0, 0, -10);
+    }
+}
